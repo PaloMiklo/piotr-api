c516d420b06d9802597f3e404292c496f1ec0cfd
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-04-08 11:44:49 +0200

    Cart [add opposite relation to cart lines]

--- src/main/java/com/api/piotr/entity/Cart.java
+++ src/main/java/com/api/piotr/entity/Cart.java
@@ -1,6 +1,7 @@
 package com.api.piotr.entity;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -48,4 +50,7 @@ public class Cart {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "order_id", referencedColumnName = "id", nullable = false)
     private OrderTable orderTable;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private Set<CartLine> lines;
 }
