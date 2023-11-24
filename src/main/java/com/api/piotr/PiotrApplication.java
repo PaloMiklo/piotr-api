d52a564524f627dee34bdd21a99f46a6b24bd4a9
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-11-25 00:02:14 +0100

    Uncomment setting images on runtime

--- src/main/java/com/api/piotr/PiotrApplication.java
+++ src/main/java/com/api/piotr/PiotrApplication.java
@@ -1,57 +1,57 @@
 package com.api.piotr;
 
-// import java.io.IOException;
-// import java.nio.file.Files;
-// import java.nio.file.Paths;
-// import java.util.List;
-// import java.util.Set;
-// import java.util.stream.Collectors;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
-// import com.api.piotr.entity.ImageTable;
-// import com.api.piotr.repository.ImageRepository;
+import com.api.piotr.entity.ImageTable;
+import com.api.piotr.repository.ImageRepository;
 
 @SpringBootApplication
-// public class PiotrApplication implements CommandLineRunner {
-public class PiotrApplication {
-
-	// @Autowired
-	// private ImageRepository imageRepository;
+public class PiotrApplication implements CommandLineRunner {
+	// public class PiotrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiotrApplication.class, args);
 	}
 
-	// @Override
-	// @Transactional
-	// public void run(String... args) throws IOException {
-	// // persist images as bytea on mocked data
-	// imageRepository.updateAll(findAllImages().stream()
-	// .map(this::updateImage)
-	// .collect(Collectors.toList()));
-	// }
+	@Autowired
+	private ImageRepository imageRepository;
 
-	// private List<ImageTable> findAllImages() {
-	// return imageRepository.findAllById(Set.of(1L, 2L, 3L));
-	// }
+	@Override
+	@Transactional
+	public void run(String... args) throws IOException {
+		// persist images as bytea on mocked data
+		imageRepository.updateAll(findAllImages().stream()
+				.map(this::updateImage)
+				.collect(Collectors.toList()));
+	}
 
-	// private ImageTable updateImage(ImageTable image) {
-	// image.setImage(getImageBytes(image));
-	// return image;
-	// }
+	private List<ImageTable> findAllImages() {
+		return imageRepository.findAllById(Set.of(1L, 2L, 3L));
+	}
 
-	// private byte[] getImageBytes(ImageTable e) {
-	// try {
-	// return Files.readAllBytes(Paths.get(String.format("images/product%s.jpg",
-	// e.getId())));
-	// } catch (IOException ex) {
-	// throw new RuntimeException("Error during extracting bytes from an image",
-	// ex);
-	// }
-	// }
+	private ImageTable updateImage(ImageTable image) {
+		image.setImage(getImageBytes(image));
+		return image;
+	}
+
+	private byte[] getImageBytes(ImageTable e) {
+		try {
+			return Files.readAllBytes(Paths.get(String.format("images/product%s.jpg",
+					e.getId())));
+		} catch (IOException ex) {
+			throw new RuntimeException("Error during extracting bytes from an image",
+					ex);
+		}
+	}
 }
--- src/main/resources/db/migration/R__create_table.sql
+++ src/main/resources/db/migration/R__create_table.sql
@@ -1,4 +1,4 @@
--- checksum 3002030999
+-- checksum 3002030991
 --DROP TABLES IF EXISTS
 DROP TABLE IF EXISTS public.cart_line CASCADE;
 
