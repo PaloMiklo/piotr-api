f608df504c65d9605e8a89fb7408ba4802729ed3
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-12-25 00:16:47 +0100

    Setup multicast queues

--- src/main/java/com/api/piotr/messaging/JmsMessageConsumer.java
+++ src/main/java/com/api/piotr/messaging/JmsMessageConsumer.java
@@ -3,6 +3,7 @@ package com.api.piotr.messaging;
 import static com.api.piotr.constant.Profile.AMQ;
 import static com.api.piotr.messaging.Queue.GENERAL_QUEUE;
 import static com.api.piotr.messaging.Queue.IMAGE_QUEUE;
+import static com.api.piotr.messaging.Queue.PIOTR_AMQ_QUEUE_COMPLET;
 import static com.api.piotr.messaging.Queue.PRODUCT_QUEUE;
 
 import org.springframework.context.annotation.Profile;
@@ -15,6 +16,11 @@ import lombok.extern.slf4j.Slf4j;
 @Profile(AMQ)
 @Component
 public class JmsMessageConsumer {
+
+    @JmsListener(destination = PIOTR_AMQ_QUEUE_COMPLET)
+    public void receiveEvent(String message) {
+        log.debug("Received event: " + message);
+    }
 
     @JmsListener(destination = GENERAL_QUEUE)
     public void receiveGeneral(String message) {
--- src/main/java/com/api/piotr/messaging/Queue.java
+++ src/main/java/com/api/piotr/messaging/Queue.java
@@ -1,7 +1,11 @@
 package com.api.piotr.messaging;
 
+import static com.api.piotr.messaging.Topic.EVENT_TOPIC;
+
 public interface Queue {
     public static final String GENERAL_QUEUE = "general";
     public static final String PRODUCT_QUEUE = "product";
     public static final String IMAGE_QUEUE = "image";
+    public static final String PIOTR_AMQ_QUEUE = "piotr-amq";
+    public static final String PIOTR_AMQ_QUEUE_COMPLET = EVENT_TOPIC + "::" + PIOTR_AMQ_QUEUE;
 }
